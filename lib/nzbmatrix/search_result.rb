182109624164961dd04a60f0d997f5808575c8ef
Marc Bowes <marcbowes@gmail.com>
2012-06-30 13:14:52 +0200

    parse a response into a search result

--- lib/nzbmatrix/search_result.rb
+++ lib/nzbmatrix/search_result.rb
@@ -1,4 +1,47 @@
 module Nzbmatrix
   class SearchResult
+    
+    require "time"
+    
+    attr_accessor :id
+    attr_accessor :name
+    attr_accessor :link
+    attr_accessor :size
+    attr_accessor :index_date
+    attr_accessor :usenet_date
+    attr_accessor :category
+    attr_accessor :group
+    attr_accessor :comments
+    attr_accessor :hits
+    attr_accessor :weblink
+    attr_accessor :language
+    attr_accessor :image_url
+    attr_accessor :region
+
+    def initialize(api_response)
+      @id = api_response["NZBID"].to_i
+      @name = api_response["NZBNAME"]
+      @link = api_response["LINK"]
+      @size = api_response["SIZE"].to_f
+      @index_date = Time.parse(api_response["INDEX_DATE"]) rescue nil
+      @usenet_date = Time.parse(api_response["USENET_DATE"]) rescue nil
+      @category = api_response["CATEGORY"]
+      @group = api_response["GROUP"]
+      @comments = api_response["COMMENTS"].to_i
+      @hits = api_response["HITS"].to_i
+      @nfo = api_response["NFO"] == "yes"
+      @weblink = api_response["WEBLINK"]
+      @language = api_response["LANGUAGE"]
+      @image_url = api_response["IMAGE"]
+      @region = api_response["REGION"].to_i
+    end
+
+    def has_nfo?
+      !!@nfo
+    end
+
+    def has_image?
+      !!@image_url
+    end
   end
 end
--- spec/search_result_spec.rb
+++ spec/search_result_spec.rb
@@ -2,6 +2,43 @@ require "nzbmatrix/search_result"
 
 module Nzbmatrix
   describe SearchResult do
-    
+
+    it "maps an api response nicely" do
+      response = {
+        "NZBID" => "396552",
+        "NZBNAME" => "Dark Angel S01 WS AC3 DVDRip XviD SFM",
+        "LINK" => "nzbmatrix.com/nzb-details.php?id=396552&hit=1",
+        "SIZE" => "9628517662.72",
+        "INDEX_DATE" => "2009-02-06 21:09:44",
+        "USENET_DATE" => "2009-02-06 1:55:56",
+        "CATEGORY" => "TV > SD",
+        "GROUP" => "alt.binaries.multimedia",
+        "COMMENTS" => "1",
+        "HITS" => "849",
+        "NFO" => "yes",
+        "WEBLINK" => "http://former.imdb.com/title/tt0204993/",
+        "LANGUAGE" => "English",
+        "IMAGE" => "http://img132.imageshack.us/img132/3606/mv5bmtmxnja2mzk3nl5bml5kj5.jpg",
+        "REGION" => "0",
+      }
+
+      result = SearchResult.new(response)
+      result.id.should eq(396552)
+      result.name.should eq("Dark Angel S01 WS AC3 DVDRip XviD SFM")
+      result.link.should eq("nzbmatrix.com/nzb-details.php?id=396552&hit=1")
+      result.size.should eq(9628517662.72)
+      result.index_date.should eq(Time.parse("2009-02-06 21:09:44"))
+      result.usenet_date.should eq(Time.parse("2009-02-06 01:55:56"))
+      result.category.should eq("TV > SD")
+      result.group.should eq("alt.binaries.multimedia")
+      result.comments.should eq(1)
+      result.hits.should eq(849)
+      result.has_nfo?.should be_true
+      result.weblink.should eq("http://former.imdb.com/title/tt0204993/")
+      result.language.should eq("English")
+      result.has_image?.should be_true
+      result.image_url.should eq("http://img132.imageshack.us/img132/3606/mv5bmtmxnja2mzk3nl5bml5kj5.jpg")
+      result.region.should eq(0)
+    end
   end
 end
